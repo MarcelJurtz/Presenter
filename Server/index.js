ec1f2e683ee3cc9b0cbf4b4d51bce12b9a9bd118
MarcelJurtz <jurtzmarcel@gmail.com>
2017-05-11 22:17:05 +0200

    Add basic server functionality

--- Server/index.js
+++ Server/index.js
@@ -8,4 +8,24 @@ app.on('ready',function(){
   });
   // Remove Menu-Bar
   mainWin.setMenu(null);
+
+  const http = require('http');
+
+  const hostname = '127.0.0.1';
+  const port = 3000;
+
+
+  function handleRequest(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<input type="button" onClick="hello" value="Click me!"/>');
+  }
+
+  function hello() {
+    Console.log("Hello WOrld!");
+  }
+
+  var server = http.createServer(handleRequest);
+
+  server.listen(port);
 });
