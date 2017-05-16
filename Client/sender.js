e10d871878c0650c6a8073b86193b18c9d92ffd5
MarcelJurtz <jurtzmarcel@gmail.com>
2017-05-16 22:56:32 +0200

    Adjust client for testing

--- Client/sender.js
+++ Client/sender.js
@@ -1,4 +1,6 @@
 function sendRequest() {
+  urlx = 'http://192.168.2.110:3000/remote';
+  console.log(urlx);
   $.ajax({
     type: 'POST',
     // TODO: Fix / use data to add more functionality than forward-clicking
@@ -8,13 +10,15 @@ function sendRequest() {
     contentType: "application/json",
     dataType: 'json',
     //url: 'http://localhost:3000/do-work',
-    url: 'http://192.168.2.110:3000/act',
+    url: urlx,
     success: function(data) {
       console.log('success');
       console.log(JSON.stringify(data));
+      console.log(url);
     },
     error: function(error) {
       console.log("some error in fetching the notifications");
+      console.log(url);
     }
   });
 }
